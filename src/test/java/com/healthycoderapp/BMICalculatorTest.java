e6edbf3cc82333e0323633da63d34a0049d4e7c2
Sahan Seneviratne <sasiniseneviratne@gmail.com>
2023-05-03 11:14:23 +0100

    first test

---
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -0,0 +1,22 @@
+package com.healthycoderapp;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BMICalculatorTest {
+    @Test
+    @DisplayName("Testing isDietRecommented is true")
+    public void testingIsDietRecommentedIsTrue() {
+        //given
+        double weight = 89.0;
+        double height = 1.72;
+        //when
+        boolean recomended = BMICalculator.isDietRecommended(weight,height);
+        //then
+        assertTrue(recomended);
+       // assertTrue(BMICalculator.isDietRecommended(89.0,1.65));
+        }
+
+}
